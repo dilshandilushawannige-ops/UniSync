953c930d64209b28165938741dcb5ded867c8118
amandi Isuri <amaisu2028@gmail.com>
2026-04-04 23:54:25 +0530

    feat: add Resource entity, repository, service and controller

---
+++ src/test/java/com/smartcampus/unisync/UnisyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartcampus.unisync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnisyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
